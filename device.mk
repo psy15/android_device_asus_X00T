c180e0962f14420bbd7420c67551f33db15f2266
Paras Y <parasy152@gmail.com>
2021-03-01 09:25:50 +0530

    LOS: BringUP

--- device.mk
+++ device.mk
@@ -225,10 +225,12 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-lineage
 
 # Overlays - Enforce RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
